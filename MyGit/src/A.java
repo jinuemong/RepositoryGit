16edde2fe1e830808d96ea3abba5eefca495119f
김진우 <@DESKTOP-IL1TKEJ>
2021-06-13 18:30:46 +0900

    Merge devB branch

--- MyGit/src/A.java
+++ MyGit/src/A.java
@@ -4,7 +4,10 @@ public class A {
 	public static void main(String[] args) {
 		System.out.println("This is A");
 		System.out.println("This is A 2nd version");
-
+		
+		new B().printB();
+		System.out.println("This is B print");
+		System.out.println("This is B print2");
 	}
 
 }
--- MyGit/src/B.java
+++ MyGit/src/B.java
@@ -2,5 +2,8 @@
 public class B {
 	public void printB() {
 		System.out.println("This is B");
+		System.out.println("This is B 2");
+		System.out.println("This is B 3");
+		System.out.println("This is B 4");
 	}
 }
